e18c3f92e9295d37b94e86af13df5c7fc80abd9b
Maximus Santosa <maximussantosa@gmail.com>
2023-02-02 22:34:40 -0800

    revert test folder

--- battery_pass/src/App.js
+++ battery_pass/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     <div className="App">
-      <Router basename={"/test"}>
+      <Router>
         <Routes>
           <Route path="/" element={<Home />}>
           </Route>
